8f4906a486827b47c2c9f6c529ce5fd3e054820a
tejaldbhatti <82153829+tejaldbhatti@users.noreply.github.com>
2021-09-04 21:40:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template , request 
+from textblob import TextBlob
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('form.html')
+
+
+@app.route('/submit', methods = ['POST'])
+def form_data():
+   user_data = request.form.get('user_data')
+   blob = TextBlob(user_data)
+   singu = blob.words.singularize()
+   plural = blob.words.pluralize()            
+
+   return render_template('predict.html', data = f'singular of the word is: {singu} , plural of the word is: {plural}')
+   
+if __name__ == '__main__':
+    app.run(app.run(host='0.0.0.0', port=8080))
